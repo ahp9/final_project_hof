0a0fc8fa41707cd2fb3a795c9762c130fc45e6f5
ahp9 <ahp9@hi.is>
2024-04-16 09:56:54 +0000

    bæta testi í Deck

--- src/test/java/is/hi/hbv202g/blackJack/DeckTest.java
+++ src/test/java/is/hi/hbv202g/blackJack/DeckTest.java
@@ -2,8 +2,8 @@ package is.hi.hbv202g.blackJack;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
-import org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -21,12 +21,27 @@ public class DeckTest {
     }
 
     @Test
-    public void testDragaSpil() {
+    public void testDrawCardEmptyDeck() {
         for (int i = 0; i < 52; i++) {
             Card card = deck.drawCard();
         }
 
         assertNull(deck.drawCard());
+    }
+
+    /**
+     * Prófar hvort spil sé í stokki, .....
+     */
+    @Test
+    public void testDrawCardInDeck(){
+        Card card = new Card(Suit.SPADI, Value.TVISTUR);
+
+        for(int i = 0; i < 52; i++){
+            Card drawnCard = deck.drawCard();
+            if(drawnCard.equals(card)){
+                assertTrue(drawnCard.equals(card));
+            }
+        }
     }
     
 }
